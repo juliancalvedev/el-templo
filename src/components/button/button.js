cc509a01a3cd08c97185b076f65bb1f1d0b1a487
PamelaElin <sosapamelaelin@gmail.com>
2021-11-23 16:31:39 -0300

    mayusculas

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const button = () => {
+const Button = () => {
     return (
         <div>
-            
+              
         </div>
     )
 }
 
-export default button
+export default B
