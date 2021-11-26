80f8dc68a9b73ef4b79f4c03883f9e15b66425cd
PamelaElin <sosapamelaelin@gmail.com>
2021-11-26 16:07:25 -0300

    login con i18n funcionando

--- src/components/LanguageSelector/LanguageSelector.js
+++ src/components/LanguageSelector/LanguageSelector.js
@@ -4,25 +4,20 @@ import { setNewLangToLocalStore } from '../../i18n-lang-conf';
 
 export const LanguageSelector = () => {
 	const { t } = useTranslation();
-
+	const onChange = (e) => setNewLangToLocalStore(e.target.value);
 	return (
 		<div style={{ boxSizing: 'content-box' }}>
 			<select
 				className='form-select form-select-sm mb-3'
 				aria-label='.form-select-sm example'
 				style={{ width: '70px' }}
+				onChange={onChange}
 			>
-				<option
-					value='es'
-					onClick={() => setNewLangToLocalStore('es')}
-					defaultValue
-				>
+				<option value='es' defaultValue>
 					{t('global.spanish')}
 				</option>
 
-				<option value='en' onClick={() => setNewLangToLocalStore('en')}>
-					{t('global.english')}
-				</option>
+				<option value='en'>{t('global.english')}</option>
 			</select>
 		</div>
 	);
