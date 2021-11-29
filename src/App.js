099d04734e8e385bb29743b3f2585fa828d6059c
Sercho <diaz.sergio@protonmail.com>
2021-11-29 12:16:31 -0300

    App.js  Limpiado

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import i18n from './i18n-lang-conf';
 import RouterApp from './Router/RouterApp';
 
-import VerifiedEmail from './pages/VerifiedEmail/VerifiedEmail';
-
 function App() {
 	//Integrates i18n to the whole App.
 	i18n.options.interpolation.defaultVariables = {
@@ -14,7 +12,6 @@ function App() {
 	return (
 		<div className='App'>
 			<RouterApp />
-			<VerifiedEmail />
 		</div>
 	);
 }
