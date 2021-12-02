aceded75dfaefe7b64ae69fd5357d5d2e3e8e7b9
Tomas Calve <tomas.calve1@gmail.com>
2021-12-02 10:57:16 -0300

    subido pero con CTRL S

--- src/i18n-lang-conf/languages/en/language.js
+++ src/i18n-lang-conf/languages/en/language.js
@@ -62,16 +62,13 @@ export default {
 		welcome: {},
 		onboard1: {},
 		onboard2: {},
-<<<<<<< HEAD
 		forgotPassword1: {},
-=======
-	
+
 		forgotPassword1: {
 			title: 'Did you forget your password?',
 			emailPlaceholder: 'Email@email.com',
 			btnAccept: 'Accept',
 		},
->>>>>>> ced551ec2bae40ca9e86eef9dfc08616dd4cc61b
 		forgotPassword2: {},
 
 		passwordRecoveryForm: {
