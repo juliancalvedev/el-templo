55c20431a6ffc7080b8b282857b61560f354717a
Sercho <diaz.sergio@protonmail.com>
2021-12-09 19:11:22 -0300

    Importada FontFamily, reestilizado Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,12 @@
+import './button.scss';
+
 const Button = ({ children, onClick, title, disabled, type = 'button' }) => {
 	return (
 		<div>
 			<div className='col-sm max-auto'>
 				<button
 					disabled={disabled}
-					className='btn btn-primary'
+					className='btn btn-primary customBtn'
 					onClick={onClick}
 					type={type}
 				>
