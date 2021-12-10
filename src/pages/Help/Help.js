cdcdef0236882b5febd7c27e055eadcb913c1c11
PamelaElin <sosapamelaelin@gmail.com>
2021-12-10 09:20:47 -0300

    node-modules

--- src/pages/Help/Help.js
+++ src/pages/Help/Help.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Title from '../../components/Title/Title';
@@ -7,10 +6,20 @@ import AuxText from '../../components/AuxText/AuxText';
 const Help = () => {
 	const { t } = useTranslation();
 	return (
-		<div className='container d-flex justify-content-center align-items-center flex-column col'>
-			<Title text={t('help.main.aboutUs')} />
-			<h3>{t('help.main.elTemplo')}</h3>
-			<AuxText text={t('help.main.auxText1')} />
+		<div>
+			<div>
+				<Title text={t('help.main.aboutUs')} />
+				<h3>{t('help.main.elTemplo')}</h3>
+			</div>
+			<div>
+				<img
+					className='container d-inline-block position-absolute start-0'
+					src='https://d500.epimg.net/cincodias/imagenes/2019/05/02/lifestyle/1556809482_591063_1556811610_noticia_normal.jpg'
+				/>
+			</div>
+			<div >
+				<AuxText  text={t('help.main.auxText1')} />
+				</div>
 		</div>
 	);
 };
