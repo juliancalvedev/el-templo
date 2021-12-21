b07ddb27a5dfffd23a07430cf0f579f4c14c4f33
PamelaElin <sosapamelaelin@gmail.com>
2021-12-21 00:31:20 -0300

    accordion style1

--- src/pages/Help/HelpAccordion.js
+++ src/pages/Help/HelpAccordion.js
@@ -1,15 +1,17 @@
-import './Help.scss'
-import {useTranslation} from 'react-i18next';
+import './Help.scss';
+import { useTranslation } from 'react-i18next';
 import Button from '../../components/Button/Button';
+import HelpAccordionBlue from './HelpAccordionBlue';
 
 const HelpAccordion = () => {
-    const {t}=useTranslation();
-    return (
-			<div class='accordion ' id='accordionExample'>
+	const { t } = useTranslation();
+	return (
+		<div>
+			<div class='accordion  ' id='accordionExample'>
 				<div class='accordion-item  '>
 					<h2 class='accordion-header  ' id='headingTwo'>
 						<button
-							class='accordion-button  collapsed'
+							class='accordion-button  bg-light text-dark'
 							type='button'
 							data-bs-toggle='collapse'
 							data-bs-target='#collapseTwo'
@@ -40,7 +42,10 @@ const HelpAccordion = () => {
 							<div className='col-8'>
 								<input className='col-12'></input>
 								<input className='col-12'></input>
-                                <Button className='col align-self-end  ' title={t('help.main.btnSend')}/>
+								<Button
+									className='col align-self-end  '
+									title={t('help.main.btnSend')}
+								/>
 							</div>
 						</div>
 					</div>
@@ -48,7 +53,7 @@ const HelpAccordion = () => {
 				<div class='accordion-item'>
 					<h2 class='accordion-header' id='headingThree'>
 						<button
-							class='accordion-button collapsed'
+							class='accordion-button bg-light text-dark'
 							type='button'
 							data-bs-toggle='collapse'
 							data-bs-target='#collapseThree'
@@ -65,20 +70,21 @@ const HelpAccordion = () => {
 						data-bs-parent='#accordionExample'
 					>
 						<div class='accordion-body'>
-							<strong>This is the third item's accordion body.</strong>{' '}
-							It is hidden by default, until the collapse plugin adds the
-							appropriate classes that we use to style each element.
-							These classes control the overall appearance, as well as
-							the showing and hiding via CSS transitions. You can modify
-							any of this with custom CSS or overriding our default
-							variables. It's also worth noting that just about any HTML
-							can go within the <code>.accordion-body</code>, though the
-							transition does limit overflow.
+							<HelpAccordionBlue />
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-}
+			<div>
+				<button className='accordion-button bg-light text-dark'>
+					terminos y condiciones
+				</button>
+				<button className='accordion-button bg-light text-dark'>
+					contacto
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default HelpAccordion
+export default HelpAccordion;
