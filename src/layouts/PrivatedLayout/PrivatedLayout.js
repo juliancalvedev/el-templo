5920e43bd51d81347c0c9b8c475990f260d2d138
Sercho <diaz.sergio@protonmail.com>
2021-12-22 14:08:10 -0300

    Agregada Lógica en PrivatedLayout para que no muestre el navbar, durante Welcome.

--- src/layouts/PrivatedLayout/PrivatedLayout.js
+++ src/layouts/PrivatedLayout/PrivatedLayout.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router';
 import Navbar from '../../components/Navbar/Navbar';
 import Button from '../../components/Button/Button';
 import { logoutAction } from '../../redux/auth';
 import { getUserInfoAction } from '../../redux/user';
-import { getUserInfo } from '../../services/user';
 
 const PrivatedLayout = () => {
+	const { goals } = useSelector((store) => store.user);
+	console.log(goals);
+
 	const dispatch = useDispatch();
 	const onLogout = () => {
 		dispatch(logoutAction());
@@ -20,7 +22,7 @@ const PrivatedLayout = () => {
 		<div>
 			<Button onClick={onLogout} title='logout' />
 			<Outlet />
-			<Navbar />
+			{goals?.length === 3 && <Navbar />}
 		</div>
 	);
 };
