fb1ec2b4c858b687adde1aae16c21b082e47a047
Tomas Calve <tomas.calve1@gmail.com>
2022-01-04 10:07:42 -0300

    clase de profile Image cambiada

--- src/components/UserImage/UserImage.js
+++ src/components/UserImage/UserImage.js
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
+import './UserImage.scss';
 
 const UserImage = () => {
 	const { img } = useSelector((store) => store.user);
 	return (
 		<div>
 			<img
-				className='profile-image'
+				className='user-profile-image'
 				src={`data:image/png;base64,${img}`}
 				alt='User_Image'
 			/>
