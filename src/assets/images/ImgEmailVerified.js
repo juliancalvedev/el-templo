6ec18c6abf365f269ade9c97da23f398a61fbb01
Sercho <diaz.sergio@protonmail.com>
2022-01-06 15:38:59 -0300

    Commit parcial

--- src/assets/images/ImgEmailVerified.js
+++ src/assets/images/ImgEmailVerified.js
@@ -1,95 +1,103 @@
 const ImgCorreo = () => {
 	return (
-		<div style={{ scale: '.9' }}>
-			<svg
-				width='200'
-				height='200'
-				viewBox='200 200'
-				fill='none'
-				xmlns='http://www.w3.org/2000/svg'
-			>
-				<g filter='url(#filter0_i_2510_2030)'>
-					<circle cx='99.5' cy='99.5' r='99.5' fill='#343A40' />
-				</g>
-				<path
-					d='M162.951 64.6606V136.18C162.951 141.086 159.073 145.036 154.256 145.036H44.7515L99.5872 110.887L162.951 64.6606Z'
-					fill='#EEEDE9'
-				/>
-				<path
-					d='M36 64.4176V135.937C36 140.843 39.878 144.793 44.6952 144.793H154.2L99.3641 110.644L36 64.4176Z'
-					fill='#E6E5E2'
-				/>
-				<path
-					d='M44.772 55.1598H154.277C159.094 55.1598 162.972 59.7694 162.972 65.4951C162.972 65.4951 125.654 100.604 103.235 112.337C102.062 112.951 100.764 113.499 99.4409 113.508C98.0993 113.518 96.771 112.991 95.5787 112.376C73.138 100.8 36.0767 65.4951 36.0767 65.4951C36.0767 59.7694 39.9548 55.1598 44.772 55.1598Z'
-					fill='#E1DED5'
-				/>
+		<svg
+			width='200'
+			height='200'
+			viewBox='0 0 200 200'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g filter='url(#filter0_i_2510_2030)'>
+				<circle cx='100' cy='100' r='100' fill='#343A40' />
+			</g>
+			<path
+				d='M163.77 64.9856V136.864C163.77 141.795 159.873 145.765 155.031 145.765H44.9763L100.088 111.444L163.77 64.9856Z'
+				fill='#EEEDE9'
+			/>
+			<path
+				d='M36.1809 64.7413V136.62C36.1809 141.551 40.0784 145.52 44.9199 145.52H154.975L99.8634 111.2L36.1809 64.7413Z'
+				fill='#E6E5E2'
+			/>
+			<path
+				d='M44.9971 55.437H155.052C159.893 55.437 163.791 60.0697 163.791 65.8243C163.791 65.8243 126.286 101.109 103.754 112.902C102.575 113.519 101.271 114.069 99.9407 114.079C98.5924 114.088 97.2574 113.559 96.0591 112.941C73.5056 101.307 36.2581 65.8243 36.2581 65.8243C36.2581 60.0697 40.1557 55.437 44.9971 55.437Z'
+				fill='#E1DED5'
+			/>
+			<g clip-path='url(#clip0_2510_2030)'>
 				<g opacity='0.99'>
 					<path
 						opacity='0.99'
-						d='M179.6 66.5555C179.6 71.2532 177.734 75.7586 174.412 79.0804C171.09 82.4022 166.585 84.2683 161.887 84.2683C159.561 84.2683 157.258 83.8102 155.109 82.92C152.96 82.0299 151.007 80.7251 149.362 79.0804C147.717 77.4356 146.413 75.4829 145.523 73.3339C144.632 71.1849 144.174 68.8816 144.174 66.5555C144.174 61.8577 146.04 57.3524 149.362 54.0306C152.684 50.7088 157.189 48.8427 161.887 48.8427C166.585 48.8427 171.09 50.7088 174.412 54.0306C177.734 57.3524 179.6 61.8577 179.6 66.5555Z'
+						d='M180.503 66.89C180.503 71.6113 178.627 76.1393 175.289 79.4778C171.95 82.8163 167.422 84.6918 162.701 84.6918C160.363 84.6918 158.048 84.2314 155.888 83.3367C153.728 82.4421 151.766 81.1308 150.113 79.4778C148.46 77.8247 147.149 75.8623 146.254 73.7024C145.359 71.5426 144.899 69.2278 144.899 66.89C144.899 62.1686 146.774 57.6407 150.113 54.3022C153.451 50.9637 157.979 49.0881 162.701 49.0881C167.422 49.0881 171.95 50.9637 175.289 54.3022C178.627 57.6407 180.503 62.1686 180.503 66.89Z'
 						fill='url(#paint0_linear_2510_2030)'
 					/>
 				</g>
 				<path
-					d='M151.504 67.7262L158.528 74.7501L171.405 57.1904'
+					d='M152.266 68.0665L159.325 75.1257L172.267 57.4778'
 					stroke='white'
 					stroke-width='5'
 				/>
-				<defs>
-					<filter
-						id='filter0_i_2510_2030'
-						x='0'
-						y='0'
-						width='199'
-						height='205'
-						filterUnits='userSpaceOnUse'
-						colorInterpolationFilters='sRGB'
-					>
-						<feFlood flooOpacity='0' result='BackgroundImageFix' />
-						<feBlend
-							mode='normal'
-							in='SourceGraphic'
-							in2='BackgroundImageFix'
-							result='shape'
-						/>
-						<feColorMatrix
-							in='SourceAlpha'
-							type='matrix'
-							values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-							result='hardAlpha'
-						/>
-						<feOffset dy='6' />
-						<feGaussianBlur stdDeviation='5' />
-						<feComposite
-							in2='hardAlpha'
-							operator='arithmetic'
-							k2='-1'
-							k3='1'
-						/>
-						<feColorMatrix
-							type='matrix'
-							values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
-						/>
-						<feBlend
-							mode='normal'
-							in2='shape'
-							result='effect1_innerShadow_2510_2030'
-						/>
-					</filter>
-					<linearGradient
-						id='paint0_linear_2510_2030'
-						x1='161.887'
-						y1='48.8427'
-						x2='161.887'
-						y2='84.2683'
-						gradientUnits='userSpaceOnUse'
-					>
-						<stop offset='0.317708' stop-color='#68CFBB' />
-						<stop offset='1' stop-color='#1E8F7A' />
-					</linearGradient>
-				</defs>
-			</svg>
-		</div>
+			</g>
+			<defs>
+				<filter
+					id='filter0_i_2510_2030'
+					x='0'
+					y='0'
+					width='200'
+					height='206'
+					filterUnits='userSpaceOnUse'
+					color-interpolation-filters='sRGB'
+				>
+					<feFlood flood-opacity='0' result='BackgroundImageFix' />
+					<feBlend
+						mode='normal'
+						in='SourceGraphic'
+						in2='BackgroundImageFix'
+						result='shape'
+					/>
+					<feColorMatrix
+						in='SourceAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+						result='hardAlpha'
+					/>
+					<feOffset dy='6' />
+					<feGaussianBlur stdDeviation='5' />
+					<feComposite
+						in2='hardAlpha'
+						operator='arithmetic'
+						k2='-1'
+						k3='1'
+					/>
+					<feColorMatrix
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+					/>
+					<feBlend
+						mode='normal'
+						in2='shape'
+						result='effect1_innerShadow_2510_2030'
+					/>
+				</filter>
+				<linearGradient
+					id='paint0_linear_2510_2030'
+					x1='162.701'
+					y1='49.0881'
+					x2='162.701'
+					y2='84.6918'
+					gradientUnits='userSpaceOnUse'
+				>
+					<stop offset='0.317708' stop-color='#68CFBB' />
+					<stop offset='1' stop-color='#1E8F7A' />
+				</linearGradient>
+				<clipPath id='clip0_2510_2030'>
+					<rect
+						width='40.201'
+						height='40.201'
+						fill='white'
+						transform='translate(144.25 46.2312)'
+					/>
+				</clipPath>
+			</defs>
+		</svg>
 	);
 };
 
