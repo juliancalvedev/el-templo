6435afd70ca2096090cb0fe352418bc84930b448
Sercho <diaz.sergio@protonmail.com>
2022-01-06 22:35:38 -0300

    Problema con el scroll horizontal

--- src/pages/welcome/Welcome/Welcome.js
+++ src/pages/welcome/Welcome/Welcome.js
@@ -2,6 +2,8 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
+import './Welcome.scss';
+
 import Button from '../../../components/Button/Button';
 import { PATHS } from '../../../constants/paths';
 import Text from '../../../components/Text/Text';
@@ -16,18 +18,31 @@ const Welcome = () => {
 
 	return (
 		<MainContainer>
-			<div className='d-flex flex-column align-items-center col-11'>
-				<Text text={t('welcome.main.title')} size='5' />
-				<Text
-					text={`${t('welcome.main.hello')} ${firstName}!`}
-					size='4'
-				/>
-				<Text text={t('welcome.main.auxText')} size='3' />
-
-				<Button
-					title={t('welcome.main.btnLetsBegin')}
-					onClick={toOnboarding}
-				/>
+			<div className='welcome__container d-flex flex-column justify-content-between align-items-center col-12'>
+				<div className='welcome__backgroundImg'></div>
+				<div className='mt-5 col-11'>
+					<Text
+						text={t('welcome.main.title')}
+						size='5'
+						color='2'
+						font='2'
+					/>
+					<Text
+						text={`${t('welcome.main.hello')} ${firstName}!`}
+						size='4'
+						color='2'
+						bold
+					/>
+				</div>
+				<div className='text-center col-10'>
+					<Text text={t('welcome.main.auxText')} size='3' color='1' />
+				</div>
+				<div className='mb-5 pb-2'>
+					<Button
+						title={t('welcome.main.btnLetsBegin')}
+						onClick={toOnboarding}
+					/>
+				</div>
 			</div>
 		</MainContainer>
 	);
