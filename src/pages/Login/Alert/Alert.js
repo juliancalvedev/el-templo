5679ccbe08c119ecdb964ebdb3f14bf7bd425454
Julian <38142331+juliancalve@users.noreply.github.com>
2022-01-07 22:14:26 -0300

    useStyles in alert component

--- src/pages/Login/Alert/Alert.js
+++ src/pages/Login/Alert/Alert.js
@@ -2,24 +2,27 @@ import { useTranslation } from 'react-i18next'
 import ImgNotActivatedAccount from '../../../assets/images/ImgNotActivatedAccount'
 import Modal from '../../../components/Modal/Modal'
 import Text from '../../../components/Text/Text'
+import useStyles from './useStyles'
 
 const Alert = ({ show, onClose }) => {
 
     const { t } = useTranslation();
 
+    const styles = useStyles();
+
     return (
         <Modal show={show} onClose={onClose}>
-            <div className='d-flex flex-column justify-content-around'>
-                <div className='d-flex justify-content-around'>
-                    <div className='col-7 d-flex flex-column justify-content-center align-items-start'>
+            <div className={styles.container}>
+                <div className={styles.topContainer}>
+                    <div className={styles.titleContainer}>
                         <Text size={4} bold text={t('auth.accountNotEnabled.title')} />
                         <Text size={3} bold text={t('auth.accountNotEnabled.subtitle')} />
                     </div>
-                    <div className='col-5 d-flex justify-content-end'>
+                    <div className={styles.imgContainer}>
                         <ImgNotActivatedAccount />
                     </div>
                 </div>
-                <div className='mt-2'>
+                <div className={styles.descriptionContainer}>
                     <Text size={3} text={t('auth.accountNotEnabled.description1')}/>
                     <Text size={3} text={t('auth.accountNotEnabled.description2')}/>
                 </div>
