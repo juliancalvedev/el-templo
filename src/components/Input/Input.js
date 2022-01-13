d68cac36cf798e12d5db648a79140fa12641fbff
Tomas Calve <tomas.calve1@gmail.com>
2022-01-13 10:38:01 -0300

    sin cosas inutilizadas

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -5,52 +5,62 @@ import IconCheck from '../../assets/Icons/IconCheck';
 import Text from '../Text/Text';
 
 const ICON = {
-    eye: <IconEye />,
-    check: <IconCheck />
-}
+	eye: <IconEye />,
+	check: <IconCheck />,
+};
 
 const Input = ({
-    label,
-    value,
-    onChange,
-    type = 'text',
-    placeholder,
-    isInvalid,
-    isValid,
-    invalidText,
-    feedback,
-    transparent,
-    icon,
-    name,
-    className,
-    disabled,
-    onClickIcon = () => { }
+	label,
+	value,
+	onChange,
+	type = 'text',
+	placeholder,
+	isInvalid,
+	isValid,
+	invalidText,
+	feedback,
+	transparent,
+	icon,
+	name,
+	className,
+	disabled,
+	onClickIcon = () => {},
 }) => {
+	const styles = useStyles({ isValid, isInvalid, transparent, className });
+	const feedbackColorType = (isInvalid && 'error') || (transparent && 2);
 
-    const styles = useStyles({ isValid, isInvalid, transparent, className });
-    const feedbackColorType = (isInvalid && 'error') || (transparent && 2);
-
-    return (
-        <div className={styles.container}>
-            {label && <label className={styles.label}>{label}</label>}
-            <div className={styles.inputContent}>
-                <input
-                    disabled={disabled}
-                    onChange={onChange}
-                    type={type}
-                    name={name}
-                    className={styles.input}
-                    value={value}
-                    placeholder={placeholder}
-                />
-                <div className={styles.iconContainer}>
-                    <span onClick={onClickIcon}>{isValid ? ICON.check : ICON[icon]}</span>
-                </div>
-            </div>
-                {feedback && <Text color={feedbackColorType} size={1} justify='end' text={feedback} />}
-                {isInvalid && <Text color='error' size={1} justify='end' text={invalidText} />}
-        </div>
-    )
-}
+	return (
+		<div className={styles.container}>
+			{label && <label className={styles.label}>{label}</label>}
+			<div className={styles.inputContent}>
+				<input
+					disabled={disabled}
+					onChange={onChange}
+					type={type}
+					name={name}
+					className={styles.input}
+					value={value}
+					placeholder={placeholder}
+				/>
+				<div className={styles.iconContainer}>
+					<span onClick={onClickIcon}>
+						{isValid ? ICON.check : ICON[icon]}
+					</span>
+				</div>
+			</div>
+			{feedback && (
+				<Text
+					color={feedbackColorType}
+					size={1}
+					justify='end'
+					text={feedback}
+				/>
+			)}
+			{isInvalid && (
+				<Text color='error' size={1} justify='end' text={invalidText} />
+			)}
+		</div>
+	);
+};
 
 export default Input;
