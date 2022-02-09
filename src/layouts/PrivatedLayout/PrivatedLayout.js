ba93fecc56086df62bba54eac1ea089645e189c3
Sercho <diaz.sergio@protonmail.com>
2022-02-09 17:02:04 -0300

    Limpiado privated LAyout

--- src/layouts/PrivatedLayout/PrivatedLayout.js
+++ src/layouts/PrivatedLayout/PrivatedLayout.js
@@ -5,8 +5,6 @@ import { Outlet } from 'react-router';
 import Navbar from '../../components/Navbar/Navbar';
 import { getUserInfoAction } from '../../redux/user';
 import { SHOW_NAVBAR } from '../../constants/paths';
-import Exercise from '../../components/Exercise/Exercise';
-import MainContainer from '../../components/MainContainer/MainContainer';
 
 const PrivatedLayout = () => {
 	const dispatch = useDispatch();
@@ -34,14 +32,10 @@ const PrivatedLayout = () => {
 	}, []);
 
 	return (
-		<MainContainer back bg='1' color='2' col='12'>
-			<Exercise />
-		</MainContainer>
-
-		// <div className='col-12'>
-		// 	<Outlet />
-		// 	{showNavbar() && <Navbar />}
-		// </div>
+		<div className='col-12'>
+			<Outlet />
+			{showNavbar() && <Navbar />}
+		</div>
 	);
 };
 
