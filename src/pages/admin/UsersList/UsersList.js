8880474d39c3887798f8eb18a3c05e0d63c9bfa3
Sercho <diaz.sergio@protonmail.com>
2022-02-15 13:50:00 -0300

    Limpiado clogs

--- src/pages/admin/UsersList/UsersList.js
+++ src/pages/admin/UsersList/UsersList.js
@@ -1,16 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import UsersTable from './UsersTable/UsersTable';
 import AdminBar from '../../../components/AdminBar/AdminBar';
 import useFetch from '../../../hooks/useFetch';
 import { getUsers } from '../../../services/admin';
 
 const UsersList = () => {
+	const [test, setTest] = useState(0);
+
 	const [data, error, apiCall] = useFetch({
 		service: () => getUsers(),
 		globalLoader: true,
 	});
-
 	useEffect(() => {
+		setTest(test + 1);
 		apiCall();
 	}, []);
 
--- src/services/admin.js
+++ src/services/admin.js
@@ -2,8 +2,4 @@ import { privateGet } from '../axios/privateInstance';
 
 const baseURL = '/admin';
 
-export const getUsers = async () => {
-	return await privateGet({
-		url: `${baseURL}/users`,
-	});
-};
+export const getUsers = () => privateGet({ url: `${baseURL}/users` });
