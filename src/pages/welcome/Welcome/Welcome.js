82fff41d5792970a3a86d3fae65461d35c094dbe
Julian <38142331+juliancalve@users.noreply.github.com>
2022-05-01 18:34:48 -0300

    no cursor

--- src/pages/welcome/Welcome/Welcome.js
+++ src/pages/welcome/Welcome/Welcome.js
@@ -7,39 +7,44 @@ import { PATHS } from '../../../constants/paths';
 import Text from '../../../components/Text/Text';
 import MainContainer from '../../../components/MainContainer/MainContainer';
 import useStyles from './useStyles';
+import DivTop from '../../../components/DivTop/DivTop';
+import DivBottom from '../../../components/DivBottom/DivBottom';
 const Welcome = () => {
 	const { t } = useTranslation();
 	const navigate = useNavigate();
-	const styles=useStyles();
+	const styles = useStyles();
 	const { firstName } = useSelector((store) => store.user);
 
 	const toOnboarding = () => navigate(`/${PATHS.MAIN_GOALS}`);
 
 	return (
 		<MainContainer backgroundImg='welcome1' scroll>
-					<div className={styles.text}>
-						<Text
-							text={t('welcome.main.title')}
-							size='5'
-							color='2'
-							font='2'
-							justify='start'
-						/>
-						<Text
-							text={`${t('welcome.main.hello')} ${firstName}!`}
-							size='4'
-							color='2'
-							bold
-							justify='start'
-						/>
-					</div>
+			<DivTop>
 
-						<Text text={t('welcome.main.auxText')} size='3' color='1' />
-						<Button
-							text={t('welcome.main.btnLetsBegin')}
-							onClick={toOnboarding}
-							size={3}
-						/>
+					<Text
+						text={t('welcome.main.title')}
+						size='5'
+						color='2'
+						font='2'
+						justify='start'
+					/>
+					<Text
+						text={`${t('welcome.main.hello')} ${firstName}!`}
+						size='4'
+						color='2'
+						bold
+						justify='start'
+					/>
+				<Text text={t('welcome.main.auxText')} size='3' color='1' />
+			</DivTop>
+			<DivBottom>
+				<Button
+					text={t('welcome.main.btnLetsBegin')}
+					onClick={toOnboarding}
+					size={3}
+				/>
+			</DivBottom>
+
 		</MainContainer>
 	);
 };
