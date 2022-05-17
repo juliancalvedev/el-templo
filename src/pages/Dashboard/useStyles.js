cfe2c3cdccf8a464b067b8a7a916bccb357cf661
julian <julian.calve@gmail.com>
2022-05-17 13:56:26 -0300

    remove padding

--- src/pages/Dashboard/useStyles.js
+++ src/pages/Dashboard/useStyles.js
@@ -4,7 +4,7 @@ const useStyles = () => {
 		calendar: 'col-12 d-flex',
 		progressSummary: 'progress-summary col-12',
 		summary: 'summary',
-		seeDetails: 'see-details pb-4',
+		seeDetails: 'see-details',
 		img: 'd-flex justify-content-center',
 		bodyInfoContainer: 'body-info-container',
 		btn: 'col-10 align-self-center my-5',
