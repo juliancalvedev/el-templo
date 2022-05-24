ff3ad1de1e0d7ad688b248fb962e58f0aa0f0935
julian <julian.calve@gmail.com>
2022-05-24 00:45:56 -0300

    done

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://el-templo.herokuapp.com/api';
+// const BASE_URL = 'https://el-templo.herokuapp.com/api';
+const BASE_URL = 'https://eltemplo.herokuapp.com/api'
 // const BASE_URL = 'http://localhost:3100/api';
 
 const axiosInstance = axios.create({
