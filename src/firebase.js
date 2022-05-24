cf017745af8dfe3129a5fb263e90b51bf52ed8cb
julian <julian.calve@gmail.com>
2022-05-24 00:49:08 -0300

    added

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATwk1I0XpgS-13uiH4R82jaJMFCSq1oU8",
-  authDomain: "el-templo.firebaseapp.com",
-  projectId: "el-templo",
-  storageBucket: "el-templo.appspot.com",
-  messagingSenderId: "988486202338",
-  appId: "1:988486202338:web:378e669b4c7353f169ffbb"
+  apiKey: "AIzaSyDBbGF7BYmb8Zlb3WqZRQPRnqf2mBZLZz4",
+  authDomain: "eltemplo-4d9d9.firebaseapp.com",
+  projectId: "eltemplo-4d9d9",
+  storageBucket: "eltemplo-4d9d9.appspot.com",
+  messagingSenderId: "481208912754",
+  appId: "1:481208912754:web:cb71ba70860e3d828dabeb",
+  measurementId: "G-E6NF6HVY5F"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default { app };  
